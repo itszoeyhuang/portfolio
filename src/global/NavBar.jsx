2eaeb70c02d312eb64fe3eed6b3e4dbc50adf640
itszoeyhuang <itszoeyhuang@gmail.com>
2025-02-26 22:19:04 -0800

    Logos

--- src/global/NavBar.jsx
+++ src/global/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router'
-
+import Logo from '../global/logo-v1-vanilla.png'
 import '../global/NavBar.css'
 
 export default function NavBar(){
@@ -8,23 +8,19 @@ export default function NavBar(){
             <div className="container">
                 <div className="grid">
                     <div className="col-3">
-                        logo goes here
+                        <Link to="/" >
+                            <img src={Logo} alt="Home Logo" ></img>
+                        </Link>
                     </div>
                     <div className="col-9">
                         <ul>
-                            <li>About</li>
-                            <li>Work</li>
-                            <li>Contact</li>
+                            <Link to="/about" >About</Link>
+                            <Link to="/work" >Work</Link>
+                            <Link to="/contact" >Contact</Link>
                         </ul>
                     </div>
                 </div>
             </div>
-            {/* <nav className='nav-bar'>
-                <Link to="/" >Home</Link>
-                <Link to="/about" >About</Link>
-                <Link to="/work" >Work</Link>
-                <Link to="/contact" >Contact</Link>
-            </nav> */}
         </header>
     )
 }
